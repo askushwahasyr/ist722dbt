7e9746970bccc620e61c0453c1679b5a975f37d0
Rianne Parker <riparker@g.syr.edu>
2025-08-06 21:04:13 +0000

    updated

--- fudge_group_project/models/dim_customer.sql
+++ fudge_group_project/models/dim_customer.sql
@@ -1,42 +1,51 @@
-with stg_customers as (
+{{ config(materialized='table') }}
 
-    select
-        account_id::varchar as customerid,
-        account_email as customeremail,
-        account_firstname as customerfirstname,
-        account_lastname as customerlastname,
-        account_address as customeraddress,
-        z.zip_city as customercity,
-        z.zip_state as customerstate,
-        account_zipcode as customerzip,
-        null as customerphone,
-        null as customerfax,
-        account_opened_on,
-        'fudgeflix' as source_system
-    from {{ source('fudgeflix_v3', 'ff_accounts') }} a
-    left join {{ source('fudgeflix_v3', 'ff_zipcodes') }} z
-        on a.account_zipcode = z.zip_code
+-- 1) Stage FudgeMart customers
+with fudgemart as (
+  select
+    customer_id          as customerid,
+    customer_email       as customeremail,
+    customer_firstname   as customerfirstname,
+    customer_lastname    as customerlastname,
+    customer_address     as customeraddress,
+    customer_zip         as customerzip,
+    'fudgemart'          as source_system
+  from {{ source('fudgemart_v3', 'fm_customers') }}
+),
 
-    union all
+-- 2) Stage FudgeFlix accounts
+fudgeflix as (
+  select
+    account_id           as customerid,
+    account_email        as customeremail,
+    account_firstname    as customerfirstname,
+    account_lastname     as customerlastname,
+    account_address      as customeraddress,
+    account_zipcode      as customerzip,
+    'fudgeflix'          as source_system
+  from {{ source('fudgeflix_v3', 'ff_accounts') }}
+),
 
-    select
-        customer_id::varchar as customerid,
-        customer_email,
-        customer_firstname,
-        customer_lastname,
-        customer_address,
-        customer_city,
-        customer_state,
-        customer_zip,
-        customer_phone,
-        customer_fax,
-        null as account_opened_on,
-        'fudgemart' as source_system
-    from {{ source('fudgemart_v3', 'fm_customers') }}
-
+-- 3) Union
+stg_customers as (
+  select * from fudgemart
+  union all
+  select * from fudgeflix
 )
 
+-- 4) Final dimension with a unique surrogate key
 select
-    {{ dbt_utils.generate_surrogate_key(['customerid', 'source_system']) }} as customerkey,
-    *
+  {{ dbt_utils.generate_surrogate_key(
+       ['stg_customers.source_system', 'stg_customers.customerid']
+     )
+  }} as customerkey,
+
+  customerid,
+  customeremail,
+  customerfirstname,
+  customerlastname,
+  customeraddress,
+  customerzip,
+  source_system
+
 from stg_customers
